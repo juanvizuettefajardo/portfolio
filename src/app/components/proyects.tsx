6277dbb1200c5fb08c7158d4b63ec5c5561887c3
juanvizuettefajardo <juanvizuette58@gmail.com>
2025-05-15 21:39:00 -0500

    Update proyects.tsx

--- src/app/components/proyects.tsx
+++ src/app/components/proyects.tsx
@@ -1,5 +1,18 @@
 import React from "react";
 
-export default function Proyects() {
-  return <div></div>;
-}
+const proyects = () => [
+  {
+    id: 1,
+    title: "Generador JMVF",
+    description:
+      "Aplicación web para generar y administrar contraseñas seguras protegiendo tus cuentas y evitando olvidarlas.",
+    image:
+      "https://sjc.microlink.io/ikwzZ4cLK4_np95jPOSftX8n6hJv0UbHnbBEUrtTlf8meUZfIGcR4B5k0P7EKNjsqq2ypHP1ophEefPdO0E65w.jpeg",
+    tags: ["Next.js", "React", "Tailwind CSS", "LocalStorage"],
+    demoUrl: "https://generador-jmvf.vercel.app/",
+    githubUrl: "",
+    showDemoOnly: true,
+  },
+];
+
+export default proyects;
