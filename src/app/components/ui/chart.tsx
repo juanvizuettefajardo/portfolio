ae25a40e4f23cf5da9493b5531d5bb8633a4409b
juanvizuettefajardo <juanvizuette58@gmail.com>
2025-05-20 20:56:36 -0500

    seven commit

--- src/app/components/ui/chart.tsx
+++ src/app/components/ui/chart.tsx
@@ -68,7 +68,7 @@ ChartContainer.displayName = "Chart";
 
 const ChartStyle = ({ id, config }: { id: string; config: ChartConfig }) => {
   const colorConfig = Object.entries(config).filter(
-    ([_, config]) => config.theme || config.color
+    ([, config]) => config.theme || config.color
   );
 
   if (!colorConfig.length) {
@@ -83,11 +83,11 @@ const ChartStyle = ({ id, config }: { id: string; config: ChartConfig }) => {
             ([theme, prefix]) => `
 ${prefix} [data-chart=${id}] {
 ${colorConfig
-  .map(([_, itemConfig]) => {
+  .map(([, itemConfig]) => {
     const color =
       itemConfig.theme?.[theme as keyof typeof itemConfig.theme] ||
       itemConfig.color;
-    return color ? `  --color-${_}: ${color};` : null;
+    return color ? `  --color- ${color};` : null;
   })
   .join("\n")}
 }
